7f73c093788391d7d56310d4f68eef47b2cc13ae
ivan <crokobit@gmail.com>
2018-05-27 01:32:36 +0800

    update

--- keyboards/ergodox_ez/keymaps/crokobit/keymap.c
+++ keyboards/ergodox_ez/keymaps/crokobit/keymap.c
@@ -1,4 +1,4 @@
-#include "ergodox.h"
+#include "ergodox_ez.h"
 #include "debug.h"
 #include "action_layer.h"
 #include "version.h"
@@ -37,11 +37,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,          KC_Q,           KC_W,   KC_E,   KC_R,   KC_T,   KC_DOWN,
         KC_ESC,    KC_A,           KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,          KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_LGUI,
-        KC_LEFT,KC_DOWN,KC_LALT,KC_LCTRL,KC_BSPC,
+        KC_LEFT,KC_DOWN,KC_LALT,KC_LCTRL,MO(1),
 
                                                     KC_LALT,   KC_HOME,
                                                                KC_END,
-                                               KC_SPC,MO(1) ,KC_LCTRL,
+                                               KC_SPC,KC_BSPC ,KC_LCTRL,
         // right hand
              KC_DELETE,      KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
              KC_UP,    KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_EQL,
@@ -87,8 +87,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                KC_TRNS,KC_TRNS,KC_TRNS,
        // right hand
        KC_TRNS, KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,
-       KC_TRNS, KC_UP,   KC_7,   KC_8,    KC_9,    KC_ASTR, KC_F12,
-                KC_DOWN, KC_4,   KC_5,    KC_6,    KC_PLUS, KC_TRNS,
+       KC_TRNS, KC_PGUP,   KC_7,   KC_8,    KC_9,    KC_ASTR, KC_F12,
+                KC_PGDN, KC_4,   KC_5,    KC_6,    KC_PLUS, KC_TRNS,
        KC_TRNS, KC_AMPR, KC_1,   KC_2,    KC_3,    KC_BSLS, KC_TRNS,
                          KC_TRNS,KC_DOT,  KC_0,    KC_EQL,  KC_TRNS,
        KC_TRNS, KC_TRNS,

64a5c1efd3f918d26471893d23c19bf05a8027cc
ivan.ho <ivan.ho@elevatelimited.com>
2019-07-30 11:50:30 +0800

    A

--- keyboards/ergodox_ez/keymaps/crokobit/keymap.c
+++ keyboards/ergodox_ez/keymaps/crokobit/keymap.c
@@ -13,20 +13,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |   `~   |   1  |   2  |   3  |   4  |   5  |  \|  |           |  =+  |   6  |   7  |   8  |   9  |   0  |  -_    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |CapsLock|   Q  |   W  |   E  |   R  |   T  | L1   |           | L2   |   Y  |   U  |   I  |   O  |   P  |  DEL   |
+ * |CapsLock|   Q  |   W  |   E  |   R  |   T  |      |           |      |   Y  |   U  |   I  |   O  |   P  |  DEL   |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | Cmd/Esc|   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;   |'"/ Cmd |
- * |--------+------+------+------+------+------|  [/  |           |  ]/  |------+------+------+------+------+--------|
- * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |shift |           |shift |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
+ * | Cmd/Esc|   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;   |        |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * | LShift |Z     |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |      | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | LCTRL|      |      | LEFT | RIGHT|                                       |  Up  | down |      |      | RCTRL|
+ *   |      |      |      |      |      |                                       |      |      |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        |      |      |       | Alt  |        |
+ *                                        |      | Home |       | PgUp |  Alt   |
  *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      | Home |       | PgUp |        |      |
- *                                 | Space| BSPC |------|       |------|  Tab   |Enter |
  *                                 |      |      | End  |       | PgDn |        |      |
+ *                                 | Space| BSPC |------|       |------|  Tab   |Enter |
+ *                                 |      |      |      |       |      |        |      |
  *                                 `--------------------'       `----------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,          KC_Q,           KC_W,   KC_E,   KC_R,   KC_T,   KC_DOWN,
         KC_ESC,    KC_A,           KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,          KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_LGUI,
-        KC_LEFT,KC_DOWN,KC_LALT,KC_LCTRL,MO(1),
+        KC_LEFT,KC_DOWN,KC_LGUI,KC_LCTRL,MO(1),
 
                                                     KC_LALT,   KC_HOME,
                                                                KC_END,
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_UP,    KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_EQL,
                           KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,          KC_QUOT,
              KC_RGUI, KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,   KC_RSFT,
-                                  MO(1),KC_RCTRL,KC_RALT,KC_UP,KC_RIGHT,
+                                  MO(1),KC_RCTRL,KC_RGUI,KC_UP,KC_RIGHT,
 
              KC_PGUP,        KC_RALT,
              KC_PGDN,
@@ -87,8 +87,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                KC_TRNS,KC_TRNS,KC_TRNS,
        // right hand
        KC_TRNS, KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,
-       KC_TRNS, KC_UP,   KC_7,   KC_8,    KC_9,    KC_ASTR, KC_F12,
-                KC_DOWN, KC_4,   KC_5,    KC_6,    KC_PLUS, KC_TRNS,
+       KC_TRNS, KC_PGUP,   KC_7,   KC_8,    KC_9,    KC_ASTR, KC_F12,
+                KC_PGDN, KC_4,   KC_5,    KC_6,    KC_PLUS, KC_TRNS,
        KC_TRNS, KC_AMPR, KC_1,   KC_2,    KC_3,    KC_BSLS, KC_TRNS,
                          KC_TRNS,KC_DOT,  KC_0,    KC_EQL,  KC_TRNS,
        KC_TRNS, KC_TRNS,

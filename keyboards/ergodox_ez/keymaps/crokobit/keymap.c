0708bdec4b9f6cad5afb8fe61eaab53dd3501ee6
ivan <crokobit@gmail.com>
2019-08-11 16:25:35 +0800

    A

--- keyboards/ergodox_ez/keymaps/crokobit/keymap.c
+++ keyboards/ergodox_ez/keymaps/crokobit/keymap.c
@@ -44,21 +44,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,          KC_Q,           KC_W,   KC_E,   KC_R,   KC_T,   KC_DOWN,
         KC_ESC,    KC_A,           KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,          KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_LGUI,
-        KC_LEFT,KC_DOWN,KC_LGUI,KC_LCTRL,MO(1),
+        KC_LEFT,KC_DOWN,KC_LBRC,KC_RBRC,KC_RCTRL,
 
                                                     KC_LALT,   KC_HOME,
                                                                KC_END,
-                                               KC_SPC,KC_BSPC ,MO(1),
+                                               KC_SPC,MT(MOD_RCTL, KC_BSPC) ,KC_TRNS,
         // right hand
              KC_DELETE,      KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
              KC_UP,    KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_EQL,
                           KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,          KC_QUOT,
              KC_RGUI, KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,   KC_RSFT,
-                                  MO(1),KC_RCTRL,KC_RGUI,KC_UP,KC_RIGHT,
+                                  KC_LCTRL,KC_PIPE,KC_RGUI,KC_UP,KC_RIGHT,
 
              KC_PGUP,        KC_RALT,
              KC_PGDN,
-             MO(1),KC_TAB, KC_ENT
+             KC_TRNS,MT(MOD_RCTL, KC_TAB), KC_ENT
     ),
 /* Keymap 1: Symbol Layer
  *
@@ -86,8 +86,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        // left hand
        M(0),   KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_TRNS,
        KC_TRNS,KC_EXLM,KC_AT,  KC_LCBR,KC_RCBR,KC_PIPE,KC_TRNS,
-       KC_TRNS,KC_HASH,KC_DLR, KC_LPRN,KC_RPRN,KC_GRV,
-       KC_TRNS,KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,KC_TRNS,
+       KC_TRNS,KC_PGUP,KC_DLR, KC_LPRN,KC_RPRN,KC_GRV,
+       KC_TRNS,KC_PGDN,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,KC_TRNS,
        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
                                        KC_TRNS,KC_TRNS,
                                                KC_TRNS,
